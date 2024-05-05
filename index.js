b38b4729d303aecc5be962a577196b238a0adde8
lexand41 <lexand41s@gmail.coml>
2024-05-05 23:00:29 +0700

    third commit

--- index.js
+++ index.js
@@ -1,26 +1,83 @@
 import "@/scss/index.scss";
 
-console.log("object");
-// import javascriptLogo from './javascript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.js'
+// const header = document.querySelector(".header");
+// const body = document.body;
+// let headerHeight = header.offsetHeight;
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
+// window.addEventListener("resize", () => {
+//   headerHeight = header.offsetHeight;
+// });
 
-// setupCounter(document.querySelector('#counter'))
+// window.addEventListener("scroll", () => {
+//   const scrollDistance = window.scrollY;
+
+//   if (scrollDistance > 200) {
+//     header.classList.add("header_fixed");
+//     body.style.paddingTop = `${headerHeight}px`;
+//   } else {
+//     header.classList.remove("header_fixed");
+//     body.style.paddingTop = "0";
+//   }
+// });
+
+const adjustElementPosition = (element, count = 0) => {
+  const rect = element.getBoundingClientRect();
+  const viewportWidth = window.innerWidth;
+
+  if (rect.left < 0) {
+    element.style.left = "0";
+    element.style.right = "auto";
+    element.style.transform = "translateX(0)";
+  } else if (rect.right > viewportWidth) {
+    element.style.left = "auto";
+    element.style.right = "0";
+    element.style.transform = "translateX(0)";
+  } else {
+    element.style.left = "50%";
+    element.style.right = "auto";
+    element.style.transform = "translateX(-50%)";
+  }
+
+  const postRect = element.getBoundingClientRect();
+
+  if ((postRect.left < 0 || postRect.right > viewportWidth) && count < 3) {
+    count++;
+    adjustElementPosition(element, count);
+  }
+};
+
+// const choicesBtns = document.querySelectorAll(".choices__btn");
+// const choicesBoxes = document.querySelectorAll(".choices__box");
+
+// for (let i = 0; i < choicesBtns.length; i++) {
+//   choicesBtns[i].addEventListener("click", () => {
+//     for (let a = 0; a < choicesBoxes.length; a++) {
+//       choicesBoxes[a].classList.remove("choices__box_open");
+//     }
+
+//     choicesBoxes[i].classList.toggle("choices__box_open");
+
+//     adjustElementPosition(choicesBoxes[i]);
+
+//     window.addEventListener("resize", () => {
+//       adjustElementPosition(choicesBoxes[i]);
+//     });
+//   });
+// }
+
+const choices = document.querySelectorAll(".choices");
+
+choices.forEach((choice) => {
+  const btn = choice.querySelector(".choices__btn");
+  const box = choice.querySelector(".choices__box");
+
+  btn.addEventListener("click", () => {
+    box.classList.toggle("choices__box_open");
+
+    adjustElementPosition(box);
+  });
+
+  window.addEventListener("resize", () => {
+    adjustElementPosition(box);
+  });
+});
